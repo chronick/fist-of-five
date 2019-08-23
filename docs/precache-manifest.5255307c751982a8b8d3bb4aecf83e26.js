7094acf2113516e2d52c555c9859609fce66ac01
Nick Donohue <nickdonohue@stockx.com>
2019-08-23 10:04:31 -0400

    add docs build

---
+++ docs/precache-manifest.5255307c751982a8b8d3bb4aecf83e26.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ef20dceecaf9467412ab06763b921a1e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c34c5be4d1a54d164c52",
+    "url": "/static/css/main.eee0177c.chunk.css"
+  },
+  {
+    "revision": "2d42f038e1964ddd56ac",
+    "url": "/static/js/2.a52f8158.chunk.js"
+  },
+  {
+    "revision": "c34c5be4d1a54d164c52",
+    "url": "/static/js/main.683b9edc.chunk.js"
+  },
+  {
+    "revision": "eccb9db3befe27ef6eac",
+    "url": "/static/js/runtime~main.2a809a0b.js"
+  }
+]);
